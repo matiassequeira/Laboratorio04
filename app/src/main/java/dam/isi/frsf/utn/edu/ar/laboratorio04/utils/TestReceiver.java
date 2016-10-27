81819bbb0ee92bf0c4f5e51985377427afe1aa88
Guille Jacob <guillejacob1@gmail.com>
2016-10-27 13:22:41 -0300

    Falta solo acomodar

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/utils/TestReceiver.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/utils/TestReceiver.java
@@ -5,9 +5,13 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import java.net.URI;
 
 import dam.isi.frsf.utn.edu.ar.laboratorio04.ListarReservasActivity;
 import dam.isi.frsf.utn.edu.ar.laboratorio04.MainActivity;
@@ -42,14 +46,18 @@ public class TestReceiver extends BroadcastReceiver {
 
             Intent i= new Intent(context, ListarReservasActivity.class);
             i.putExtra("usuario",usuario);
-            //PendingIntent pi = PendingIntent.getActivity(context, 0, i, 0);
             PendingIntent pi = PendingIntent.getActivity(context, reserva.getId(), i, PendingIntent.FLAG_ONE_SHOT);
+
+            //seteando ringotne a la notificacion
+            RingtoneManager rinmanager = null;
+            Uri uri = rinmanager.getRingtoneUri(rinmanager.TYPE_NOTIFICATION);
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext())
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentIntent(pi)
                     .setContentTitle("Confirmacion reserva")
-                    .setContentText("La reserva ha sido confirmada!");
+                    .setContentText("La reserva ha sido confirmada!")
+                    .setSound(uri);
 
             notifmanager.notify(1, builder.build());
         }
