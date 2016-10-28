e3cea6a2c4478ccc68db22dd2cddb671d7090fde
Guille Jacob <guillejacob1@gmail.com>
2016-10-27 21:48:04 -0300

    arreglos en preferencias

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/PreferenciasActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/PreferenciasActivity.java
@@ -1,9 +1,13 @@
 package dam.isi.frsf.utn.edu.ar.laboratorio04;
 
+import android.app.Activity;
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PreferenciasActivity extends AppCompatActivity {
+public class PreferenciasActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/PreferenciasFragment.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/PreferenciasFragment.java
@@ -1,20 +1,39 @@
 package dam.isi.frsf.utn.edu.ar.laboratorio04;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PreferenciasFragment extends PreferenceFragment {
-
+    EditTextPreference userEditText;
+    EditTextPreference emailEditText;
+    RingtonePreference ringtonepref;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String user = sp.getString("usuario","usuario default");
+        String mail = sp.getString("email","email default");
+        String ringtone = sp.getString("ringtone","ningun ringtone");
+
+        userEditText = (EditTextPreference) findPreference("usuario");
+        userEditText.setSummary(user);
+
+        emailEditText = (EditTextPreference) findPreference("email");
+        emailEditText.setSummary(mail);
+
+        ringtonepref = (RingtonePreference) findPreference("ringtone");
+        ringtonepref.setSummary(ringtone);
 
     }
+
+
 }
